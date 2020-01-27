924364fe101452d1870f71e6c4c02efe0dadf330
Jacinda Zhong <jacindaz@gmail.com>
2020-01-26 19:57:45 -0800

    Whoops, fix db create method

--- db_operations/database.py
+++ db_operations/database.py
@@ -8,28 +8,16 @@ class Database:
 
     def create(self):
         """
-        If database already exists:
-          -> error
-
-        If not:
-          -> if specifies schema, and schema
-             does not exist, error
-          -> create new directory in public
+        If database already exists: error
+        If not: create new directory in public
         """
 
-        schema_exists = os.path.isdir(f"data_files/{self.schema}")
-        if schema_exists:
-            database_exists = os.path.isdir(f"data_files/{self.schema}/{self.name}")
-            if database_exists:
-                logging.error("database already exists")
-                exception = Exception()
-                exception.msg = "database already exists"
-                return exception
-            else:
-                os.mkdir(f"data_files/{self.schema}/{self.name}")
-                logging.info("Created database!")
-        else:
-            logging.error(f"schema {self.schema} does not exist")
+        database_exists = os.path.isdir(f"data_files/{self.schema}/{self.name}")
+        if database_exists:
+            logging.error("database already exists")
             exception = Exception()
-            exception.msg = f"schema {self.schema} does not exist"
+            exception.msg = "database already exists"
             return exception
+        else:
+            os.mkdir(f"data_files/{self.schema}/{self.name}")
+            logging.info("Created database!")
--- process.py
+++ process.py
@@ -12,13 +12,7 @@ def process(data):
         operation = parsed_result[0]
         entity = parsed_result[1]
         db_name = parsed_result[2]
-        if operation == "create" and entity == "database":
-            name_and_schema = db_name.split(".")
-            if len(name_and_schema) == 2:
-                db_name = name_and_schema[0]
-                schema_name = name_and_schema[1]
-                db = Database(db_name, schema=schema_name)
-            else:
-                db = Database(db_name)
 
-            return db.create()
+        if operation == "create" and entity == "database":
+            db = Database(db_name)
+            db.create()
